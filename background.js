14ce29b87c2b3a1de96af5e311df5dae0dad47fc
kuvanov <yu.kuwano@smarthr.co.jp>
2023-08-10 11:15:46 +0900

    add multiple supertags feature

--- background.js
+++ background.js
@@ -1,37 +1,36 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.storage.sync.get(["targetNodeId", "superTagId", "endpoint", "apiToken"], (data) => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "pageInfo") {
+    chrome.storage.sync.get(["targetNodeId", "superTagIds"], (data) => {
       const payload = {
         targetNodeId: data.targetNodeId || "",
         nodes: [
           {
             name: `[${tab.title}](${tab.url})`,
-            supertags: [
-              {
-                id: data.superTagId || "",
-              },
-            ],
+            supertags: data.superTagIds.map((id) => ({ id: id })),
           },
         ],
       };
-  
-      const endpoint = data.endpoint || "https://europe-west1-tagr-prod.cloudfunctions.net/addToNodeV2"; // デフォルトエンドポイント
+
+      const endpoint =
+        data.endpoint ||
+        "https://europe-west1-tagr-prod.cloudfunctions.net/addToNodeV2"; // デフォルトエンドポイント
       const token = data.apiToken || ""; // API Token
-  
+
       fetch(endpoint, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify(payload)
+        body: JSON.stringify(payload),
       })
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           console.log("POST successful:", data);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error("POST error:", error);
         });
     });
-  });
-  
+  }
+});
--- options.js
+++ options.js
@@ -1,27 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("options-form");
   const saveButton = document.getElementById("save-button");
+  const addSuperTagButton = document.getElementById("addSuperTag");
+  const superTagInputs = document.getElementById("superTagInputs");
+
+  addSuperTagButton.addEventListener("click", () => {
+    const input = document.createElement("input");
+    input.type = "text";
+    input.name = "superTagId";
+    superTagInputs.appendChild(input);
+  });
 
   saveButton.addEventListener("click", () => {
     const endpoint = document.getElementById("endpoint").value;
     const apiToken = document.getElementById("apiToken").value;
     const targetNodeId = document.getElementById("targetNodeId").value;
-    const superTagId = document.getElementById("superTagId").value;
+    const superTagIdInputs = document.getElementsByName("superTagId");
+    const superTagIds = Array.from(superTagIdInputs).map(input => input.value).filter(value => value !== "");
 
-    chrome.storage.sync.set({
-      endpoint: endpoint,
-      apiToken: apiToken,
-      targetNodeId: targetNodeId,
-      superTagId: superTagId
-    }, () => {
-      console.log("Options saved:", endpoint, apiToken, targetNodeId, superTagId);
-    });
+    chrome.storage.sync.set(
+      {
+        endpoint: endpoint,
+        apiToken: apiToken,
+        targetNodeId: targetNodeId,
+        superTagIds: superTagIds,
+      },
+      () => {
+        console.log(
+          "Options saved:",
+          endpoint,
+          apiToken,
+          targetNodeId,
+          superTagIds
+        );
+      }
+    );
   });
 
-  chrome.storage.sync.get(["endpoint", "apiToken", "targetNodeId", "superTagId"], (data) => {
-    document.getElementById("endpoint").value = data.endpoint || "";
-    document.getElementById("apiToken").value = data.apiToken || "";
-    document.getElementById("targetNodeId").value = data.targetNodeId || "";
-    document.getElementById("superTagId").value = data.superTagId || "";
-  });
+  chrome.storage.sync.get(
+    ["endpoint", "apiToken", "targetNodeId", "superTagId"],
+    (data) => {
+      document.getElementById("endpoint").value = data.endpoint || "";
+      document.getElementById("apiToken").value = data.apiToken || "";
+      document.getElementById("targetNodeId").value = data.targetNodeId || "";
+      document.getElementById("superTagId").value = data.superTagId || "";
+    }
+  );
 });
